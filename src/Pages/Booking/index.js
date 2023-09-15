44c76289e83fa6170d5981912682673db057f895
Ilyass Baba <ilyassbaba.me@gmail.com>
2023-09-15 19:40:21 +0100

    Booking working and needs to look better (no delete)

--- src/Pages/Booking/index.js
+++ src/Pages/Booking/index.js
@@ -12,13 +12,95 @@ function Booking() {
       .then((response) => setRes(response));
   }, []);
 
+  function AddBooking(){
+
+    const Start =  document.querySelector(".Date-Booking").value + " " + document.querySelector(".Start-Booking").value + ":00"
+    const End =  document.querySelector(".Date-Booking").value + " " + document.querySelector(".End-Booking").value + ":00"
+    const Data = {
+      "callsign" : document.querySelector(".Callsign-Booking").value,
+      // "cid" : sessionStorage.getItem("CID"),
+      "cid" : "1674212",
+      "start" : Start,
+      "end" : End,
+      "type": document.querySelector(".Type-Booking").value,
+      "division": "MENA",
+      "subdivision": "MAG"
+    }
+    alert(JSON.stringify(Data))
+
+    fetch('http://127.0.0.1:1000/AddMaghrebBooking', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        Data
+      })
+    });
+  }
+
   return (
     <>
       <SideBar />
       <div className="Booking PagesContainer">
         <UpperBar Username={sessionStorage.getItem("FullName")} />
 
+
+
+
+
         <div className="Bookings">
+          <div className="Bookings-Add">
+            <div className="Bookings-Add-Title"><h1>New Booking</h1></div>
+
+            <div className="Bookings-Add-Inputs">
+              <div>
+                <p>CallSign</p>
+                <input className="Callsign-Booking" type="text" placeholder="Callsign"></input>
+              </div>
+              <div>
+                <p>Day</p>
+                <input className="Date-Booking" type="date" placeholder="start"></input>
+              </div>
+              <div>
+                <p>Start</p>
+                <input className="Start-Booking" type="time" placeholder="start"></input>
+              </div>
+              <div>
+                <p>End</p>
+                <input className="End-Booking" type="time" placeholder="end"></input>
+              </div>
+              <div>
+                <p>Type</p>
+                <select className="Type-Booking">
+                  <option>booking</option>
+                </select>
+                {/* <input type="text" placeholder="type"></input> */}
+              </div>
+              <div>
+                <p>CID</p>
+                <input className="CID-Booking" type="text" placeholder="1674212" disabled></input>
+              </div>
+              <div>
+                <p>Division</p>
+                <input className="DIV-Booking" type="text" placeholder="MENA" disabled></input>
+              </div>
+              <div>
+                <p>Subdivision</p>
+                <input className="SUBDIV-Booking" type="text" placeholder="MAG" disabled></input>
+              </div>
+              <div className="Bookings-Add-Inputs-submit">
+                <input type="submit"  onClick={AddBooking}></input>
+              </div>
+                
+                
+                
+               
+                
+              
+            </div>
+          </div>
+
           <div className="Bookings-Container">
             <div className="Bookings-Container-Name">
               <div>
