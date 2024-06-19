c5399ad1de884c0bef2281b8a68a20162c421aa5
Ilyass Baba <ilyassbaba.me@gmail.com>
2024-06-19 21:52:21 +0100

    SYNC

--- src/Pages/Component/UpperBar/index.js
+++ src/Pages/Component/UpperBar/index.js
@@ -6,6 +6,7 @@ import MenuIconComponent from "./Components/Menu"
 import pdp from "../../../Ressources/pdp.jpg"
 
 function UpperBar(props){
+    const Logout = (process.env.REACT_APP_APP_ENV == "PROD") ? "http://api.vatsim.ma:3001" : "http://localhost:3001"
     return(
         <div className="UpperBar">
             <div className="UpperBarMenu">
